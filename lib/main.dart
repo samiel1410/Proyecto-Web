e17de76521564fba3eaca1cad51334320e3f021f
samiel1410 <samiel_141099@hotmail.com>
2021-09-07 16:21:10 -0500

    Notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:pagosapp_group/providers/app_provider.dart';
@@ -9,10 +11,37 @@ import 'package:pagosapp_group/src/pages/main_page.dart';
 import 'package:pagosapp_group/src/pages/resgiter_page.dart';
 import 'package:pagosapp_group/src/utils/user_shared_preferences.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/foundation.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
 
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    channel = const AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
   await Preferences().init();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider<GoalElementProvider>(
@@ -20,9 +49,48 @@ Future<void> main() async {
     ChangeNotifierProvider<AppProvider>(create: (_) => AppProvider())
   ], child: MyApp()));
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
   final prefs = new Preferences();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage().then((value) {
+      print('An initial message event was published!');
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('A new onMessage event was published!');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                icon: '@mipmap/ic_launcher',
+              ),
+            ));
+      }
+
+      print(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      print(message);
+    });
+  }
   
  Widget build(BuildContext context) {
     final appProvider = Provider.of<AppProvider>(context, listen: true);
