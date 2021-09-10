688592beca0eb5bd2b8c1ca3969c0438df4f6619
alivel1216 <akvelastegui1@espe.edu.ec>
2021-09-10 00:49:07 -0500

    Line Chart v1

--- lib/src/models/expense_model.dart
+++ lib/src/models/expense_model.dart
@@ -9,40 +9,39 @@ Expense expenseFromJson(String str) => Expense.fromJson(json.decode(str));
 String expenseToJson(Expense data) => json.encode(data.toJson());
 
 class Expense {
-    Expense({
-         this.idexpense,
-       required this.categori,
-       required this.description,
-       required this.amount,
-       required this.date,
-      required  this.address,
-        required this.image,
-        this.photo
-    });
+  Expense(
+      {this.idexpense,
+      required this.categori,
+      required this.description,
+      required this.amount,
+      required this.date,
+      required this.address,
+      required this.image,
+      this.photo});
 
-    Expense.create(this.categori, this.description,this.amount, this.date, this.address , this.image );
+  Expense.create(this.categori, this.description, this.amount, this.date,
+      this.address, this.image);
 
-    String? idexpense;
-    String categori;
-    String description;
-    int amount;
-    DateTime date;
-    String address;
-    String image;
-    String? photo;
+  String? idexpense;
+  String categori;
+  String description;
+  int amount;
+  DateTime date;
+  String address;
+  String image;
+  String? photo;
 
-    factory Expense.fromJson(Map<String, dynamic> json) => Expense(
-        idexpense: json["idexpense"],
-        categori: json["categori"],
-        description: json["description"],
-        amount:  json["amount"],
-        date: DateTime.parse(json["date"]),
-        address: json["address"],
-        image: json["image"],
-        photo: json["photo"]
-    );
+  factory Expense.fromJson(Map<String, dynamic> json) => Expense(
+      idexpense: json["idexpense"],
+      categori: json["categori"],
+      description: json["description"],
+      amount: json["amount"],
+      date: DateTime.parse(json["date"]),
+      address: json["address"],
+      image: json["image"],
+      photo: json["photo"]);
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "idexpense": idexpense,
         "categori": categori,
         "description": description,
@@ -52,5 +51,5 @@ class Expense {
         "address": address,
         "image": image,
         "photo": photo,
-    };
+      };
 }
